db960fbf0661c0b54e1d4eeeb5abdcfac46674cf
ggibelli <giogibelli@icloud.com>
2020-07-13 15:51:03 +1200

    exercises 6.1-6.8

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <AnecdoteForm />
+      <AnecdoteList  />
+    </div>
+  )
+}
+
+export default App
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div>
+      <div>{anecdote.content}</div>
+      <div>has {anecdote.votes} <button onClick={handleClick}>vote</button></div>
+    </div>
+  )
+}
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+
+  return (
+    <div>
+      {anecdotes.map(anecdote =>
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => dispatch(voteAnecdote(anecdote.id))}
+        />
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
